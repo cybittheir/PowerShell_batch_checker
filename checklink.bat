803747ab114f34a07041191fca6b50a90e7a89ac
cybittheir <kibitzer.vl@gmail.com>
2022-12-12 08:29:17 +1000

    errorlevel added

--- checklink.bat
+++ checklink.bat
@@ -3,5 +3,9 @@ md C:\TEMP
 
 ipconfig | findstr IPv4 > C:\TEMP\tmp_ip.txt
 
-pwsh -ExecutionPolicy Bypass -File "c:\LED\#RD\CheckLink.ps1"
 powershell -ExecutionPolicy Bypass -File "c:\LED\#RD\CheckLink.ps1"
+if errorlevel 0 goto end
+
+pwsh -ExecutionPolicy Bypass -File "c:\LED\#RD\CheckLink.ps1"
+
+:end
